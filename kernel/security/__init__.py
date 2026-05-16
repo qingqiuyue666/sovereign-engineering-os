05d7261eb337b6982ae1aa8bb90993da93da538b
qqyqqyqqy666-wq <qqyqqyqqy666@gmail.com>
2026-05-17 02:59:44 +0800

    Add security package init

---
+++ kernel/security/__init__.py
@@ -0,0 +1,10 @@
+"""Security primitives for V12 leak prevention."""
+
+__all__ = [
+    "security_classification",
+    "secret_scanner",
+    "environment_sanitizer",
+    "anti_exfiltration_gate",
+    "ai_context_firewall",
+    "repository_hygiene",
+]
