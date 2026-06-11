af5df2d20fb008f133a1990b69852ff04c17f1fd
qqyqqyqqy666-wq <qqyqqyqqy666@gmail.com>
2026-06-11 22:35:32 +0800

    Export knowledge proposal ingest helper

--- kernel/knowledge/__init__.py
+++ kernel/knowledge/__init__.py
@@ -7,6 +7,7 @@ as the authority layer.
 
 from kernel.knowledge.graph import build_workspace_graph, write_workspace_graph
 from kernel.knowledge.object_model import KnowledgeObject, KnowledgeRelation
+from kernel.knowledge.proposal_ingest import ingest_proposal_note
 from kernel.knowledge.vault import export_task_to_vault, export_workspace_to_vault, init_control_vault
 from kernel.knowledge.vault_scanner import scan_control_vault
 
@@ -16,6 +17,7 @@ __all__ = [
     "build_workspace_graph",
     "export_task_to_vault",
     "export_workspace_to_vault",
+    "ingest_proposal_note",
     "init_control_vault",
     "scan_control_vault",
     "write_workspace_graph",
