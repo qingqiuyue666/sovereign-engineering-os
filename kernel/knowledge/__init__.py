5a8604d8a77d959ff004281b27153bc298f3d718
qqyqqyqqy666-wq <qqyqqyqqy666@gmail.com>
2026-06-11 22:26:26 +0800

    Export control vault scanner

--- kernel/knowledge/__init__.py
+++ kernel/knowledge/__init__.py
@@ -8,6 +8,7 @@ as the authority layer.
 from kernel.knowledge.graph import build_workspace_graph, write_workspace_graph
 from kernel.knowledge.object_model import KnowledgeObject, KnowledgeRelation
 from kernel.knowledge.vault import export_task_to_vault, export_workspace_to_vault, init_control_vault
+from kernel.knowledge.vault_scanner import scan_control_vault
 
 __all__ = [
     "KnowledgeObject",
@@ -16,5 +17,6 @@ __all__ = [
     "export_task_to_vault",
     "export_workspace_to_vault",
     "init_control_vault",
+    "scan_control_vault",
     "write_workspace_graph",
 ]
